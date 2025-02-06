4608506eda260bda905c79debcf658425747bb12
daveLegende <davelalegende5@gmail.com>
2025-02-06 13:14:45 +0000

    add prolongation

--- src/otp/adapter/module/otp.module.ts
+++ src/otp/adapter/module/otp.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { IOtpService } from 'src/otp/app/module';
 import { OtpService } from './otp.service';
 import { OtpController } from './otp.controller';
-import { OtpRepositoryModule } from 'src/otp/framework/database/Otp.repository.module';
+import { OtpRepositoryModule } from 'src/otp/framework/database/otp.repository.module';
 import { UserRepositoryModule } from 'user/framework/database/user.repository.module';
 import { AuthApiModule } from 'user/framework/API';
 import { TwilioModule } from 'src/twilio/twilio.module';
