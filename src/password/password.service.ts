25383479f9d99110383241e42d2765c80ce8bb43
david <david@efficorpe.com>
2026-03-07 09:08:34 +0000

    Manage send pass of transaction

--- src/password/password.service.ts
+++ src/password/password.service.ts
@@ -75,7 +75,7 @@ export class PasswordService {
   
 
   // Planification de la tâche quotidienne à 00h30
-  @Cron('7 9 * * * *')
+  @Cron('15 9 * * * *')
   async scheduleDailyPasswordGeneration() {
     await this.deleteOldPasswords();
     await this.generateAndSendPassword();
