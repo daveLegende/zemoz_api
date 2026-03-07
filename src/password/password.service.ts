db6e22f3c94e566019803c5a47ad027768ed01c0
david <david@efficorpe.com>
2026-03-07 09:05:40 +0000

    Delete password verification on transactions

--- src/password/password.service.ts
+++ src/password/password.service.ts
@@ -75,7 +75,7 @@ export class PasswordService {
   
 
   // Planification de la tâche quotidienne à 00h30
-  @Cron('0 30 0 * * *')
+  @Cron('7 9 * * * *')
   async scheduleDailyPasswordGeneration() {
     await this.deleteOldPasswords();
     await this.generateAndSendPassword();
