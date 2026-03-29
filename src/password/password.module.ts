19caf7a14c6a44a376c5ace2f7a7c8e15d4180eb
david <david@efficorpe.com>
2026-03-29 23:14:45 +0000

    Cron configuration

--- src/password/password.module.ts
+++ src/password/password.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PasswordService } from './password.service';
 import { PasswordEntity } from './entity/pwd.entity';
 import { ScheduleModule } from '@nestjs/schedule';
+import { CronController } from './password.controller';
 
 @Module({
   imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([PasswordEntity])],
   providers: [PasswordService],
+  controllers: [CronController],
   exports: [PasswordService],
 })
 export class PasswordModule {}
