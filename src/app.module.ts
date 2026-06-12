e55f40ecc19e59a037a7541a5cbd2eefe83a0f71
daveLegende <davelalegende5@gmail.com>
2026-06-12 12:53:45 +0000

    fix some bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -72,8 +72,8 @@ export class IAppModule {}
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.dev.env', //.dev.env, .prod.env
-      // envFilePath: '.prod.env', //.dev.env, .prod.env
+      // envFilePath: '.dev.env', //.dev.env, .prod.env
+      envFilePath: '.prod.env', //.dev.env, .prod.env
       expandVariables: true,
       isGlobal: true,
     }),
